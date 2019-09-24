4bf9cf53a1cbeec1b13fb278e463f3838b5d1f81
Dominique Skaf <doskaf@yahoo.com>
2019-09-24 18:12:31 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,7 +9,7 @@ class Patient
     @@all << self
   end
   
-  def new_appointment(date = "", doctor)
+  def new_appointment(date = "#{(/.*,.*\d*\w*/)}", doctor)
     Appointment.new(date, self, doctor)
   end
   
