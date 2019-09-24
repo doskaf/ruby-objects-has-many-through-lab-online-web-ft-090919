0c64ac88b48f6ca191e21754c713584a948b95f8
Dominique Skaf <doskaf@yahoo.com>
2019-09-24 18:15:39 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,7 +4,7 @@ class Appointment
   
   @@all = []
   
-  def initialize(date = (/.*,.*\d*\w*/), patient, date)
+  def initialize(patient, doctor, date)
     @date = date
     @patient = patient
     @doctor = doctor
